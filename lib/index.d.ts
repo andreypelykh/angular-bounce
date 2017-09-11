2adc58fed9eb4486f2c17e498636bc19fc46a75b
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-11 08:29:33 +0300

    use animation & useAnimation

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,120 +1,120 @@
-import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
+import { AnimationReferenceMetadata } from '@angular/animations';
 /**
 * Attention seekers animations
 */
-export declare const BOUNCE: AnimationKeyframesSequenceMetadata;
-export declare const FLASH: AnimationKeyframesSequenceMetadata;
-export declare const HEAD_SHAKE: AnimationKeyframesSequenceMetadata;
-export declare const JELLO: AnimationKeyframesSequenceMetadata;
-export declare const PULSE: AnimationKeyframesSequenceMetadata;
-export declare const RUBBER_BAND: AnimationKeyframesSequenceMetadata;
-export declare const SHAKE: AnimationKeyframesSequenceMetadata;
-export declare const SWING: AnimationKeyframesSequenceMetadata;
-export declare const TADA: AnimationKeyframesSequenceMetadata;
-export declare const WOBBLE: AnimationKeyframesSequenceMetadata;
+export declare const BOUNCE: AnimationReferenceMetadata;
+export declare const FLASH: AnimationReferenceMetadata;
+export declare const HEAD_SHAKE: AnimationReferenceMetadata;
+export declare const JELLO: AnimationReferenceMetadata;
+export declare const PULSE: AnimationReferenceMetadata;
+export declare const RUBBER_BAND: AnimationReferenceMetadata;
+export declare const SHAKE: AnimationReferenceMetadata;
+export declare const SWING: AnimationReferenceMetadata;
+export declare const TADA: AnimationReferenceMetadata;
+export declare const WOBBLE: AnimationReferenceMetadata;
 /**
 * Bouncing entrances animations
 */
-export declare const BOUNCE_IN: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_IN_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_IN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_IN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_IN_UP: AnimationKeyframesSequenceMetadata;
+export declare const BOUNCE_IN: AnimationReferenceMetadata;
+export declare const BOUNCE_IN_DOWN: AnimationReferenceMetadata;
+export declare const BOUNCE_IN_LEFT: AnimationReferenceMetadata;
+export declare const BOUNCE_IN_RIGHT: AnimationReferenceMetadata;
+export declare const BOUNCE_IN_UP: AnimationReferenceMetadata;
 /**
 * Bouncing exits animations
 */
-export declare const BOUNCE_OUT: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_OUT_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_OUT_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_OUT_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const BOUNCE_OUT_UP: AnimationKeyframesSequenceMetadata;
+export declare const BOUNCE_OUT: AnimationReferenceMetadata;
+export declare const BOUNCE_OUT_DOWN: AnimationReferenceMetadata;
+export declare const BOUNCE_OUT_LEFT: AnimationReferenceMetadata;
+export declare const BOUNCE_OUT_RIGHT: AnimationReferenceMetadata;
+export declare const BOUNCE_OUT_UP: AnimationReferenceMetadata;
 /**
 * Fading entrances animations
 */
-export declare const FADE_IN: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_DOWN_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_LEFT_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_RIGHT_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_UP: AnimationKeyframesSequenceMetadata;
-export declare const FADE_IN_UP_BIG: AnimationKeyframesSequenceMetadata;
+export declare const FADE_IN: AnimationReferenceMetadata;
+export declare const FADE_IN_DOWN: AnimationReferenceMetadata;
+export declare const FADE_IN_DOWN_BIG: AnimationReferenceMetadata;
+export declare const FADE_IN_LEFT: AnimationReferenceMetadata;
+export declare const FADE_IN_LEFT_BIG: AnimationReferenceMetadata;
+export declare const FADE_IN_RIGHT: AnimationReferenceMetadata;
+export declare const FADE_IN_RIGHT_BIG: AnimationReferenceMetadata;
+export declare const FADE_IN_UP: AnimationReferenceMetadata;
+export declare const FADE_IN_UP_BIG: AnimationReferenceMetadata;
 /**
 * Fading exits animations
 */
-export declare const FADE_OUT: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_DOWN_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_LEFT_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_RIGHT_BIG: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_UP: AnimationKeyframesSequenceMetadata;
-export declare const FADE_OUT_UP_BIG: AnimationKeyframesSequenceMetadata;
+export declare const FADE_OUT: AnimationReferenceMetadata;
+export declare const FADE_OUT_DOWN: AnimationReferenceMetadata;
+export declare const FADE_OUT_DOWN_BIG: AnimationReferenceMetadata;
+export declare const FADE_OUT_LEFT: AnimationReferenceMetadata;
+export declare const FADE_OUT_LEFT_BIG: AnimationReferenceMetadata;
+export declare const FADE_OUT_RIGHT: AnimationReferenceMetadata;
+export declare const FADE_OUT_RIGHT_BIG: AnimationReferenceMetadata;
+export declare const FADE_OUT_UP: AnimationReferenceMetadata;
+export declare const FADE_OUT_UP_BIG: AnimationReferenceMetadata;
 /**
 * Flippers animations
 */
-export declare const FLIP: AnimationKeyframesSequenceMetadata;
-export declare const FLIP_IN_X: AnimationKeyframesSequenceMetadata;
-export declare const FLIP_IN_Y: AnimationKeyframesSequenceMetadata;
-export declare const FLIP_OUT_X: AnimationKeyframesSequenceMetadata;
-export declare const FLIP_OUT_Y: AnimationKeyframesSequenceMetadata;
+export declare const FLIP: AnimationReferenceMetadata;
+export declare const FLIP_IN_X: AnimationReferenceMetadata;
+export declare const FLIP_IN_Y: AnimationReferenceMetadata;
+export declare const FLIP_OUT_X: AnimationReferenceMetadata;
+export declare const FLIP_OUT_Y: AnimationReferenceMetadata;
 /**
 * Lightspeed animations
 */
-export declare const LIGHT_SPEED_IN: AnimationKeyframesSequenceMetadata;
-export declare const LIGHT_SPEED_OUT: AnimationKeyframesSequenceMetadata;
+export declare const LIGHT_SPEED_IN: AnimationReferenceMetadata;
+export declare const LIGHT_SPEED_OUT: AnimationReferenceMetadata;
 /**
 * Rotating entrances animations
 */
-export declare const ROTATE_IN: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_IN_DOWN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_IN_DOWN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_IN_UP_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_IN_UP_RIGHT: AnimationKeyframesSequenceMetadata;
+export declare const ROTATE_IN: AnimationReferenceMetadata;
+export declare const ROTATE_IN_DOWN_LEFT: AnimationReferenceMetadata;
+export declare const ROTATE_IN_DOWN_RIGHT: AnimationReferenceMetadata;
+export declare const ROTATE_IN_UP_LEFT: AnimationReferenceMetadata;
+export declare const ROTATE_IN_UP_RIGHT: AnimationReferenceMetadata;
 /**
 * Rotating exits animations
 */
-export declare const ROTATE_OUT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_OUT_DOWN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_OUT_DOWN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_OUT_UP_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ROTATE_OUT_UP_RIGHT: AnimationKeyframesSequenceMetadata;
+export declare const ROTATE_OUT: AnimationReferenceMetadata;
+export declare const ROTATE_OUT_DOWN_LEFT: AnimationReferenceMetadata;
+export declare const ROTATE_OUT_DOWN_RIGHT: AnimationReferenceMetadata;
+export declare const ROTATE_OUT_UP_LEFT: AnimationReferenceMetadata;
+export declare const ROTATE_OUT_UP_RIGHT: AnimationReferenceMetadata;
 /**
 * Sliding entrances animations
 */
-export declare const SLIDE_IN_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_IN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_IN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_IN_UP: AnimationKeyframesSequenceMetadata;
+export declare const SLIDE_IN_DOWN: AnimationReferenceMetadata;
+export declare const SLIDE_IN_LEFT: AnimationReferenceMetadata;
+export declare const SLIDE_IN_RIGHT: AnimationReferenceMetadata;
+export declare const SLIDE_IN_UP: AnimationReferenceMetadata;
 /**
 * Sliding exits animations
 */
-export declare const SLIDE_OUT_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_OUT_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_OUT_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const SLIDE_OUT_UP: AnimationKeyframesSequenceMetadata;
+export declare const SLIDE_OUT_DOWN: AnimationReferenceMetadata;
+export declare const SLIDE_OUT_LEFT: AnimationReferenceMetadata;
+export declare const SLIDE_OUT_RIGHT: AnimationReferenceMetadata;
+export declare const SLIDE_OUT_UP: AnimationReferenceMetadata;
 /**
 * Specials animations
 */
-export declare const HINGE: AnimationKeyframesSequenceMetadata;
-export declare const JACK_IN_THE_BOX: AnimationKeyframesSequenceMetadata;
-export declare const ROLL_IN: AnimationKeyframesSequenceMetadata;
-export declare const ROLL_OUT: AnimationKeyframesSequenceMetadata;
+export declare const HINGE: AnimationReferenceMetadata;
+export declare const JACK_IN_THE_BOX: AnimationReferenceMetadata;
+export declare const ROLL_IN: AnimationReferenceMetadata;
+export declare const ROLL_OUT: AnimationReferenceMetadata;
 /**
 * Zooming entrances animations
 */
-export declare const ZOOM_IN: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_IN_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_IN_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_IN_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_IN_UP: AnimationKeyframesSequenceMetadata;
+export declare const ZOOM_IN: AnimationReferenceMetadata;
+export declare const ZOOM_IN_DOWN: AnimationReferenceMetadata;
+export declare const ZOOM_IN_LEFT: AnimationReferenceMetadata;
+export declare const ZOOM_IN_RIGHT: AnimationReferenceMetadata;
+export declare const ZOOM_IN_UP: AnimationReferenceMetadata;
 /**
 * Zooming exits animations
 */
-export declare const ZOOM_OUT: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_OUT_DOWN: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_OUT_LEFT: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_OUT_RIGHT: AnimationKeyframesSequenceMetadata;
-export declare const ZOOM_OUT_UP: AnimationKeyframesSequenceMetadata;
+export declare const ZOOM_OUT: AnimationReferenceMetadata;
+export declare const ZOOM_OUT_DOWN: AnimationReferenceMetadata;
+export declare const ZOOM_OUT_LEFT: AnimationReferenceMetadata;
+export declare const ZOOM_OUT_RIGHT: AnimationReferenceMetadata;
+export declare const ZOOM_OUT_UP: AnimationReferenceMetadata;
